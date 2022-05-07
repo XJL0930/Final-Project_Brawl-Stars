730869c76388548254f6f73d7a8833eb230fc188
XJL0930 <2928004597@qq.com>
2022-05-07 11:48:07 +0800

    5.7 update
    5.7 更新了BattleScene类和desert_map的资源文件

---
+++ Classes/BattleScene.cpp
@@ -0,0 +1,57 @@
+#include"BattleScene.h"
+
+BattleScene* BattleScene::create(int testIndex /* = 1 */)
+{
+	switch (testIndex){
+	    case 1:{
+		    auto scene = new(std::nothrow) BattleScene("Scene/desert_map/desert_map.tmx");
+		    if (scene && scene->init())
+		    {
+			    scene->autorelease();
+			    return scene;
+		    }
+		    else
+		    {
+			    delete scene;
+			    scene = nullptr;
+			    return nullptr;
+		    }
+		    break;
+	    }
+	    default:
+			break;
+	}
+	return nullptr;
+}
+
+bool BattleScene::init()
+{
+	if (!(this->Scene::init() && this->Scene::initWithPhysics()))
+		return false;
+	//this->setTag(0);
+	this->addChild(battlemap,0);
+	return true;
+}
+
+Scene* BattleScene::createScene()
+{
+	auto scene = Scene::createWithPhysics();
+	if (scene != nullptr)
+	{
+		scene->addChild(this, 0);
+		scene->getPhysicsWorld()->setAutoStep(true);
+		scene->getPhysicsWorld()->setGravity(cocos2d::Vec2::ZERO);
+		scene->retain();
+		return scene;
+	}
+	return nullptr;
+}
+
+BattleScene::BattleScene(std::string mapname/*还要继续加入人物类的data member的初始化*/) {
+	battlemap=TMXTiledMap::create(mapname);
+
+	this->meta_barrier = battlemap->getLayer("meta_barrier");
+	this->meta_barrier->setVisible(false);
+	this->meta_grass = battlemap->getLayer("meta_grass");
+	this->meta_grass->setVisible(false);
+};
---
+++ Resources/Scene/desert_map/buch-outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="buch-outdoor" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="55" columns="11">
+ <image source="buch-outdoor.png" width="384" height="192"/>
+</tileset>
